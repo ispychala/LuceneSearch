6d064cc7b59e8e5c9e04063522d81a862573629e
Inga Spychala <ingaspychala@gmail.com>
2020-01-15 23:38:47 +0100

    display score

--- src/main/java/com/ztpd/projekt/Searcher.java
+++ src/main/java/com/ztpd/projekt/Searcher.java
@@ -32,67 +32,11 @@ public class Searcher {
         Analyzer analyzer = new StandardAnalyzer();
 
         ArrayList<Result> results;
-
-        // TERM QUERY
-        // A Query that matches documents containing a term.
-        // This may be combined with other terms with a BooleanQuery.
-       /* String queryMammal = "MaMMal";
-        TermQuery tq1;
-        {
-            System.out.println("1) term query: mammal (CONTENT)");
-            queryMammal = analyzer.normalize(queryMammal, queryMammal).utf8ToString();
-            Term t1 = new Term(Constants.content, queryMammal);
-            tq1 = new TermQuery(t1);
-
-            results = printResultsForQuery(indexSearcher, tq1);
-        }
 /*
-        String queryBird = "bird";
-        TermQuery tq2;
-        {
-            System.out.println("2) term query bird (CONTENT)");
-            queryBird = analyzer.normalize(queryBird, queryBird).utf8ToString();
-            Term t2 = new Term(Constants.content, queryBird);
-            tq2 = new TermQuery(t2);
-
-            printResultsForQuery(indexSearcher, tq2);
-        }
-
+        // TERM QUERY
         // Boolean query
-        {
-            System.out.println("3) boolean query (CONTENT): mammal or bird");
-            BooleanQuery.Builder builder = new BooleanQuery.Builder();
-            builder.add(tq1, BooleanClause.Occur.SHOULD);
-            builder.add(tq2, BooleanClause.Occur.SHOULD);
-            builder.setMinimumNumberShouldMatch(1);
-            BooleanQuery boolquery = builder.build();
-
-            printResultsForQuery(indexSearcher, boolquery);
-        }
-
-        // Range query.
-        {
-            System.out.println("4) range query: file size in [0b, 1000b]");
-            Query rangequery = IntPoint.newRangeQuery(Constants.filesize_int, 0, 1000);
-            printResultsForQuery(indexSearcher, rangequery);
-        }
-
         // PrefixQuery.
-        {
-            System.out.println("5) Prefix query (FILENAME): ant");
-            Term prefix = new Term(Constants.filename, "ant");
-            PrefixQuery prefixquery = new PrefixQuery(prefix);
-            printResultsForQuery(indexSearcher, prefixquery);
-        }
-
         // WildcardQuery
-        {
-            System.out.println("6) Wildcard query (CONTENT): eat?");
-            Term wildcard = new Term(Constants.content, "eat?");
-            WildcardQuery wildcardquery = new WildcardQuery(wildcard);
-            printResultsForQuery(indexSearcher, wildcardquery);
-        }
-
         // FuzzyQuerry
         {
             System.out.println("7) Fuzzy querry (CONTENT): mamml?");
@@ -111,7 +55,7 @@ public class Searcher {
         {
             System.out.println("8) query parser = " + query);
             Query q = new QueryParser(Constants.content, analyzer).parse(query);
-            results = printResultsForQuery(indexSearcher, q);
+            results = getResultsForQuery(indexSearcher, q);
         }
 
         try {
@@ -123,7 +67,7 @@ public class Searcher {
         return results;
     }
 
-    private static ArrayList<Result> printResultsForQuery(IndexSearcher indexSearcher, Query q) throws IOException {
+    private static ArrayList<Result> getResultsForQuery(IndexSearcher indexSearcher, Query q) throws IOException {
 
         TopDocs topdocs = indexSearcher.search(q, Constants.top_docs);
         ArrayList<Result> results = new ArrayList<>();
